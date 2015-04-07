0e5b7aca0f23796d1432e08332d97e15a81e6772
Philipp Bosch <hello@pb.io>
2015-04-07 18:15:13 +0200

    Add dist folder.

---
+++ dist/scripts/index.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("rbmaSushiSequencer",[]).controller("SushiSequencerController",function(){return console.log("Controller here."),console.log("!!!!!!!!!")}).directive("rbmaSushiSequencer",function(){return{template:"This is the sushi directive"}});
